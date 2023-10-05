961abd4f4f584c4e9c412f02d86cd9ee64717c18
ABHINAND S S <91078614+abhinandshaji@users.noreply.github.com>
2023-10-05 17:27:58 +0530

    Add files via upload

--- src/my_cpp_pkg/src/turtle_control_node.cpp
+++ src/my_cpp_pkg/src/turtle_control_node.cpp
@@ -9,7 +9,7 @@ class TurtleControlNode : public rclcpp::Node
 public:
     TurtleControlNode() : Node("turtle_control_node")
     {
-        // Subscribe to turtle_position
+        // Subscribe to turrtle_position
         subscriber_ = this->create_subscription<turtlesim::msg::Pose>("turtle_position", 10,
                                                                       std::bind(&TurtleControlNode::callbackGoal,
                                                                                 this, std::placeholders::_1));
@@ -23,7 +23,7 @@ public:
         status_publisher_ = this->create_publisher<std_msgs::msg::Bool>("turtle_status", 10);
 
         // Initialize shared pointers
-        er_ = std::make_shared<turtlesim::msg::Pose>();
+        // er_ = std::make_shared<turtlesim::msg::Pose>();
         goal_ = std::make_shared<turtlesim::msg::Pose>();
 
         RCLCPP_INFO(this->get_logger(), "Turtle control node has been started!!");
@@ -38,6 +38,8 @@ private:
     void callbackPos(const turtlesim::msg::Pose::SharedPtr pos_)
     {
         // Get to goal
+        // error
+        // Since they are shared ptr we use -> syntax
         // pose runs from pi to -pi
         // goal from pi to -pi
         er_->theta = goal_->theta - pos_->theta;
@@ -49,9 +51,9 @@ private:
         // if only first turtle there we dont let it move
         if (goal_->x == 0) // We made it so that we get only 1-10 values for goal so this condition enough to fix that this is the first turtle
         {
-            V_x = 0.0f;
-            V_y = 0.0f;
-            omega_ = 0.0f;
+            error_x = 0.0f;
+            error_y = 0.0f;
+            theta_er = 0.0f;
         }
 
         // Check if next iteration required
@@ -63,36 +65,38 @@ private:
             status_.data = false;
 
             // Since next iteration is not needed set values to zero
-            V_x = 0.0f;
-            V_y = 0.0f;
-            omega_ = 0.0f;
+            error_x = 0.0f;
+            error_y = 0.0f;
+            theta_er = 0.0f;
         }
 
         else
-        {   
+        {
             auto status_ = std_msgs::msg::Bool();
             status_.data = false;
             status_publisher_->publish(status_);
-            V_x = er_->x / dt_;
-            V_y = er_->y / dt_;
-            omega_ = er_->theta / dt_;
+            error_x = er_->x / dt_;
+            error_y = er_->y / dt_;
+            theta_er = er_->theta / dt_;
         }
 
+        // we  use . syntax as send_pos_ is not shared pointer
         send_pos_.angular.x = 0.0f;
         send_pos_.angular.y = 0.0f;
         send_pos_.linear.z = 0.0f;
 
-        // Using PID control set value of input pose_
+        // Using PD control
         // goal x and goal y are interchanged by the velicty axis and axis of the turtle
-        send_pos_.angular.z = 0.006 * (omega_) + 0.00045 * omega_ * dt_ + 0.00003 * (omega_ / dt_);
+        // NOTE: Final Desired angular velocity is zero, hence no desired velocity is present
+        send_pos_.angular.z = 0.006 * (theta_er) + 0.00045 * theta_er * dt_ + 0.00003 * (theta_er / dt_);
 
-        /* Using rotation matrix equations use
-        the Vx, Vy as the global x_dot and y_dot and
-        what we send as the body velocities u, v, r*/
-        // this tuning for the PID control works best till now
-        //Poles of this system is has negative real parts and one zero at origin so this system is stable
-        send_pos_.linear.x = cos(pos_->theta) * ((0.1 * (V_x) + 0.0006 * V_x * dt_ + 0.00002 * (V_x / dt_))) + sin(pos_->theta) * ((0.1 * (V_y) + 0.0006 * V_y * dt_ + 0.00002 * (V_y / dt_)));
-        send_pos_.linear.y = -sin(pos_->theta) * ((0.1 * (V_x) + 0.0006 * V_x * dt_ + 0.00002 * (V_x / dt_))) + cos(pos_->theta) * ((0.1 * (V_y) + 0.0006 * V_y * dt_ + 0.00002 * (V_y / dt_)));
+        /* Using rotation matrix equations and using
+           the Vx, Vy as the global x_dot and y_dot
+           we send as the body velocities u, v, r    */
+        // Poles of this system is has negative real parts and one zero at origin so this system is stable
+        // NOTE: Final Desired linear velocity is zero, hence no desired velocity term is present
+        send_pos_.linear.x = cos(pos_->theta) * ((0.05 * (error_x) + 0.000002 * (error_x / dt_))) + sin(pos_->theta) * ((0.05 * (error_y) + 0.000002 * (error_y / dt_)));
+        send_pos_.linear.y =-sin(pos_->theta) * ((0.05 * (error_x) + 0.000002 * (error_x / dt_))) + cos(pos_->theta) * ((0.05 * (error_y) + 0.000002 * (error_y / dt_)));
 
         pub_->publish(send_pos_);
     }
@@ -104,17 +108,17 @@ private:
     rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr parent_sub_;
 
     // set variable for error and time
-    // std::shared_ptr<turtlesim::msg::Pose> er_;
+    std::shared_ptr<turtlesim::msg::Pose> er_;
     float dt_;
 
     // Publisher varibale and the variable to publish
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
     geometry_msgs::msg::Twist send_pos_;
 
-    //variables to be send through topics
-    float V_x;
-    float V_y;
-    float omega_;
+    // variables to be send through topics
+    float error_x;
+    float error_y;
+    float theta_er;
 
     // finish status publisher
     rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr status_publisher_;
